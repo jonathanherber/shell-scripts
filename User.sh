3d43900c20be9c5e4e269208f021b5e4c5155720
Jonathan Herber <jonathanherber15@gmail.com>
2022-05-01 19:55:11 -0300

    up

--- User.sh
+++ User.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+ls /home/$1 > /dev/null 2>&1 || { echo "Usuario Inexistente" ; exit 1; }
+
 USER=$1
 ID=$(id -u $USER)
 HOME=$(du /home/$1 -sh)
+LOGIN=$(lastlog -u $USER)
 
 
+#$(grep $1 /etc/passwd |cut -d":" -f3)
+#$(grep $1 /etc/passwd |cut -d":" -f5 | tr -d ,)
+#$(du -sh /hom/$1 |cut -f1)
 
 echo "UID: $ID"
 echo ""
 echo "Total: $HOME"
+echo ""
+echo "Ultimo login: $LOGIN"
+
